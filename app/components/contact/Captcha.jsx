c06582372e1cb6eed23dc8ff03b1c65b0a45e46f
Kishan89 <kishan895737@gmail.com>
2025-07-01 10:57:59 +0530

    update

--- app/components/contact/Captcha.jsx
+++ app/components/contact/Captcha.jsx
@@ -3,11 +3,27 @@ import ReCAPTCHA from "react-google-recaptcha";
 export default function Captcha({ onVerify, error }) {
   return (
     <div className="mb-6">
-      <div className="transform scale-100 sm:scale-90 xs:scale-[0.85] origin-0">
-        <ReCAPTCHA
-          sitekey="6LcwEnMrAAAAAOwFGJVHd49Qf7N-czaZys1zRe6_"
-          onChange={onVerify}
-        />
+      {/* Wrapping in a container to scale captcha based on screen size */}
+      <div className="w-full overflow-hidden">
+        <div
+          className="
+            transform 
+            scale-[0.77] 
+            sm:scale-[0.88] 
+            md:scale-[0.95] 
+            lg:scale-100 
+            origin-top-left 
+            w-[300px] 
+            sm:w-[330px] 
+            md:w-[370px] 
+            lg:w-[400px]
+          "
+        >
+          <ReCAPTCHA
+            sitekey="6LcwEnMrAAAAAOwFGJVHd49Qf7N-czaZys1zRe6_"
+            onChange={onVerify}
+          />
+        </div>
       </div>
       {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
     </div>
